a1683c856af3019aff57dcf7b0402b93a12f0585
Daniel Broe <dbroe1995@gmail.com>
2024-07-04 20:10:05 -0400

    Add signup functionality

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,6 +1,22 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useSignup from "../hooks/useSignup";
 
 const SignUp = () => {
+	const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const { loading, signup } = useSignup();
+
+	const handleSubmitForm = (e: React.FormEvent) => {
+		e.preventDefault();
+		signup(inputs);
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
 			<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,7 +24,7 @@ const SignUp = () => {
 					Sign Up <span className="text-blue-500"> ChatApp</span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmitForm}>
 					<div>
 						<label className="label p-2">
 							<span className="text-base label-text">Full Name</span>
@@ -17,6 +33,10 @@ const SignUp = () => {
 							type="text"
 							placeholder="Full Name"
 							className="w-full input input-bordered  h-10"
+							value={inputs.fullName}
+							onChange={(e) =>
+								setInputs({ ...inputs, fullName: e.target.value })
+							}
 						/>
 					</div>
 
@@ -28,6 +48,10 @@ const SignUp = () => {
 							type="text"
 							placeholder="Username"
 							className="w-full input input-bordered h-10"
+							value={inputs.username}
+							onChange={(e) =>
+								setInputs({ ...inputs, username: e.target.value })
+							}
 						/>
 					</div>
 
@@ -39,6 +63,10 @@ const SignUp = () => {
 							type="password"
 							placeholder="Password"
 							className="w-full input input-bordered h-10"
+							value={inputs.password}
+							onChange={(e) =>
+								setInputs({ ...inputs, password: e.target.value })
+							}
 						/>
 					</div>
 
@@ -50,6 +78,10 @@ const SignUp = () => {
 							type="password"
 							placeholder="Confirm Password"
 							className="w-full input input-bordered h-10"
+							value={inputs.confirmPassword}
+							onChange={(e) =>
+								setInputs({ ...inputs, confirmPassword: e.target.value })
+							}
 						/>
 					</div>
 
@@ -61,8 +93,12 @@ const SignUp = () => {
 					</Link>
 
 					<div>
-						<button className="btn btn-block btn-sm mt-2 border border-slate-700">
-							Sign Up
+						<button
+							type="submit"
+							className="btn btn-block btn-sm mt-2 border border-slate-700"
+							disabled={loading}
+						>
+							{loading ? "Loading..." : "Sign Up"}
 						</button>
 					</div>
 				</form>
